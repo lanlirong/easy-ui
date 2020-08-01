4407c7b84497510a85ab3e3df91d991dca9804f9
lanlirong <lanlirong@qutoutiao.net>
2020-08-01 08:46:40 +0800

    使用webpack

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// import Button from "@/components/button.vue";
+// import Icon from "@/components/icon.vue";
+// import ButtonGroup from "@/components/button-group.vue";
+// Vue.component('e-button', Button)
+// Vue.component('e-icon', Icon)
+// Vue.component('e-button-group', ButtonGroup)
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <e-button>按钮</e-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import Vue from "vue";
 
+import eButton from "@/components/button.vue";
+// import ButtonGroup from "@/components/button-group.vue";
+// Vue.component('e-button', Button)
+// Vue.component('e-icon', Icon)
+// Vue.component('e-button-group', ButtonGroup)
+
+// import  Button from  './components/button'
+// import  Icon from  './components/icon'
+// import ButtonGroup from './components/button-group'
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    eButton
   }
 };
 </script>
